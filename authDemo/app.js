77e3e12245dfa32a313843ad7819bf5763f413ed
Carlos Ramirez <carlosjramirez11@gmail.com>
2019-02-21 20:37:47 -0800

    Imported passport and created model

--- authDemo/app.js
+++ authDemo/app.js
@@ -3,9 +3,30 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/auth_demo_app', {
   useNewUrlParser: true
 });
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local');
+const User = require('./models/user');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(
+  require('express-session')({
+    secret: 'Harley is the best pup',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('home');

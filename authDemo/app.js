d1c1dbb04f490fefd85248bdd6378c96482ad004
cjislegit <carlosjramirez11@gmail.com>
2019-02-22 14:07:38 -0800

    Added register form and auth

--- authDemo/app.js
+++ authDemo/app.js
@@ -11,6 +11,7 @@ const User = require('./models/user');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   require('express-session')({
     secret: 'Harley is the best pup',
@@ -28,12 +29,40 @@ passport.deserializeUser(User.deserializeUser());
 app.use(passport.initialize());
 app.use(passport.session());
 
+//==============
+//ROUTES
+//==============
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/secret', (req, res) => {
   res.render('secret');
+});
+
+//Auth Routes
+
+//Show sign upform
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+//Handaling user sign up
+app.post('/register', (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.render('register');
+      }
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secret');
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
